f23161a42e8751b2f0b97c5e736b969f2d1b6a10
Emiliano García <emiliano.garciadavila@hotmail.com>
2022-05-01 19:07:53 -0500

    GUI improvement.

--- src/main/java/GUI/Interfaz.java
+++ src/main/java/GUI/Interfaz.java
@@ -3,6 +3,8 @@ package GUI;
 import static PIA.TuringMachine.*;
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.Desktop;
+import java.net.URI;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
@@ -11,7 +13,6 @@ public class Interfaz extends javax.swing.JFrame {
     public Interfaz() {
         initComponents();
         this.setLocationRelativeTo(null);
-        this.setBackground(Color.WHITE);
         ImageIcon imagenUANL= new ImageIcon("UANL.png");
         UANLlabel.setIcon(new ImageIcon(imagenUANL.getImage().getScaledInstance(UANLlabel.getWidth(), UANLlabel.getHeight(), Image.SCALE_SMOOTH)));
         ImageIcon imagenFCFM= new ImageIcon("FCFM.png");
@@ -29,16 +30,36 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("TA 032 PIA E1");
+        setForeground(java.awt.Color.darkGray);
+        setMinimumSize(new java.awt.Dimension(475, 380));
+        setPreferredSize(new java.awt.Dimension(300, 308));
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        IngresoCadenaTextField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        getContentPane().add(IngresoCadenaTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 200, 270, -1));
+
+        ComprobarButton.setFont(new java.awt.Font("Franklin Gothic Medium Cond", 0, 18)); // NOI18N
         ComprobarButton.setText("Comprobar");
+        ComprobarButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         ComprobarButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 ComprobarButtonActionPerformed(evt);
             }
         });
+        getContentPane().add(ComprobarButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 240, 125, 45));
 
         UANLlabel.setIcon(new javax.swing.ImageIcon("D:\\OneDrive\\OneDrive - Universidad Autonoma de Nuevo León\\Teoría de Autómatas - Prof. Yazmany Jahaziel Guerrero Ceja\\TA-032-PIA-E1\\UANL.png")); // NOI18N
         UANLlabel.setMaximumSize(new java.awt.Dimension(60, 60));
@@ -46,6 +67,7 @@ public class Interfaz extends javax.swing.JFrame {
         UANLlabel.setName(""); // NOI18N
         UANLlabel.setPreferredSize(new java.awt.Dimension(60, 60));
         UANLlabel.setRequestFocusEnabled(false);
+        getContentPane().add(UANLlabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(18, 22, 78, 100));
 
         FCFMlabel.setIcon(new javax.swing.ImageIcon("D:\\OneDrive\\OneDrive - Universidad Autonoma de Nuevo León\\Teoría de Autómatas - Prof. Yazmany Jahaziel Guerrero Ceja\\TA-032-PIA-E1\\FCFM.png")); // NOI18N
         FCFMlabel.setMaximumSize(new java.awt.Dimension(60, 60));
@@ -53,70 +75,67 @@ public class Interfaz extends javax.swing.JFrame {
         FCFMlabel.setName(""); // NOI18N
         FCFMlabel.setPreferredSize(new java.awt.Dimension(60, 60));
         FCFMlabel.setRequestFocusEnabled(false);
+        getContentPane().add(FCFMlabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(371, 22, 72, 70));
 
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("PRODUCTO INTEGRADOR");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(3, 22, 470, -1));
 
+        jLabel2.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 18)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("DE APRENDIZAJE");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 50, 470, -1));
 
         jLabel3.setFont(new java.awt.Font("Consolas", 1, 24)); // NOI18N
-        jLabel3.setText("L={ a²ⁿbbb | n≥1 }");
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("L = {a²ⁿbbb|n≥1}");
         jLabel3.setAlignmentX(0.5F);
         jLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 132, 470, 50));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(UANLlabel, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
-                        .addComponent(jLabel1)
-                        .addGap(53, 53, 53))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(76, 76, 76)
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addComponent(FCFMlabel, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(IngresoCadenaTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(58, 58, 58))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(131, 131, 131)
-                        .addComponent(ComprobarButton, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(81, 81, 81)
-                        .addComponent(jLabel3)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(UANLlabel, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(FCFMlabel, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(22, 22, 22)
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel2)))
-                .addGap(26, 26, 26)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
-                .addComponent(IngresoCadenaTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(ComprobarButton, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(43, 43, 43))
-        );
+        jButton1.setFont(new java.awt.Font("Franklin Gothic Medium Cond", 0, 18)); // NOI18N
+        jButton1.setText("Simulador");
+        jButton1.setAlignmentX(0.5F);
+        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButton1.setPreferredSize(new java.awt.Dimension(125, 28));
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 240, -1, 45));
+
+        jLabel4.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("MÁQUINA DE TURING");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 96, 470, 30));
+
+        jMenu1.setText("Equipo 1");
+
+        jMenuItem2.setText("José Antonio Cano Caramillo - 1914173");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Emiliano García Dávila - 1958339");
+        jMenu1.add(jMenuItem3);
+
+        jMenuItem5.setText("Manuel Fernando Rivera de León - 1742940");
+        jMenu1.add(jMenuItem5);
+
+        jMenuItem4.setText("Juan José Suárez Ibarra - 2076264");
+        jMenu1.add(jMenuItem4);
+
+        jMenuItem1.setText("Angel Manuel Tapia Avitia - 1905895");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -127,6 +146,19 @@ public class Interfaz extends javax.swing.JFrame {
         else
             JOptionPane.showMessageDialog(null, "Cadena inválida.");
     }//GEN-LAST:event_ComprobarButtonActionPerformed
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        try{
+            Desktop.getDesktop().browse(new URI("http://turingmachinesimulator.com/shared/rsspmwkejp"));
+        }
+        catch(Exception exc){
+            JOptionPane.showMessageDialog(null, "Error, no se pudo abrir el simulador.");
+        }
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -165,8 +197,17 @@ public class Interfaz extends javax.swing.JFrame {
     private javax.swing.JLabel FCFMlabel;
     private javax.swing.JTextField IngresoCadenaTextField;
     private javax.swing.JLabel UANLlabel;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
     // End of variables declaration//GEN-END:variables
 }
